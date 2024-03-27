b09325bc5a055a184c3c14ed30c1fed6312e0e56
Tracker-Friendly <jliwin98@danwin1210.de>
2024-03-27 18:51:52 +0000

    Test commit

---
+++ schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS userdata;
+DROP TABLE IF EXISTS sessions;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TEXT NOT NULL,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE userdata (
+    creator INTEGER NOT NULL,
+    appId TEXT NOT NULL,
+    secret TEXT NOT NULL
+);
+
+CREATE TABLE sessions (
+    sessionid INTEGER PRIMARY KEY AUTOINCREMENT,
+    session TEXT NOT NULL,
+    id INTEGER NOT NULL,
+    device TEXT NOT NULL DEFAULT "?"
+);
